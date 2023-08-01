4aebbedacac583568cbb07b783ecec9172ff0119
Justin <alexanderrr416@gmail.com>
2023-07-31 23:34:51 -0400

    Refactor: filter styles.

--- src/components/ExpenseFilter.tsx
+++ src/components/ExpenseFilter.tsx
@@ -1,29 +1,31 @@
-import { Select } from "@chakra-ui/react";
+import { Box, Select } from "@chakra-ui/react";
 import { useExpense } from "../context/ExpenseContext";
-import { useState } from "react";
 
 interface Props {
   selectedCategory: string;
-  onCategoryChange: (category: string) => void;
+  handleSelectedCategory: (category: string) => void;
 }
 
 export const ExpenseFilter = ({
   selectedCategory,
-  onCategoryChange,
+  handleSelectedCategory,
 }: Props) => {
   const { expenses } = useExpense();
 
   return (
-    <Select
-      placeholder="Filter by category"
-      value={selectedCategory}
-      onChange={(e) => onCategoryChange(e.target.value)}
-    >
-      {expenses?.map((expense) => (
-        <option key={expense.id} value={expense.category}>
-          {expense.category}
-        </option>
-      ))}
-    </Select>
+    <Box display="inline-block">
+      <Select
+        flexGrow={0}
+        placeholder="Filter by category"
+        value={selectedCategory}
+        onChange={(e) => handleSelectedCategory(e.target.value)}
+      >
+        {expenses?.map((expense) => (
+          <option key={expense.id} value={expense.category}>
+            {expense.category}
+          </option>
+        ))}
+      </Select>
+    </Box>
   );
 };
