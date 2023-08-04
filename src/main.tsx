b4c6122ee819d18819596dab9a443b40c83638ee
Justin <alexanderrr416@gmail.com>
2023-08-03 22:31:38 -0400

    Creating the theme.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
+import theme from "./theme.ts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#176357",
+    800: "#157568",
+    700: "#3A9976",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+export default theme;
