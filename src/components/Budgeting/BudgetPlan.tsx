e9d84a8cc97acc3ab8e6e04ab1efd91552b5684d
Justin <alexanderrr416@gmail.com>
2023-08-14 21:02:57 -0400

    Build the user budget plan.

--- src/components/Budgeting/BudgetPlan.tsx
+++ src/components/Budgeting/BudgetPlan.tsx
@@ -1,44 +1,103 @@
 import {
   Box,
-  HStack,
-  Progress,
+  Center,
+  CircularProgress,
+  CircularProgressLabel,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
   Stat,
   StatLabel,
   StatNumber,
+  Text,
 } from "@chakra-ui/react";
+// import { differenceInMilliseconds, startOfDay } from "date-fns";
+import { useExpense } from "../../context/ExpenseContext";
 import { useBudget } from "../../context/UserBudgetContext";
 
 export const BudgetPlan = () => {
   const { budget } = useBudget();
+  const { expenses } = useExpense();
 
   const budgetStartDate = budget?.[0]?.startDate;
   const budgetEndDate = budget?.[0]?.endDate;
+  const budgetAmount = budget?.[0]?.amount;
 
-  let progress = 0;
+  // let progress = 0;
 
-  if (budgetStartDate && budgetEndDate) {
-    const currentDate = new Date();
-    const startDate = new Date(budgetStartDate);
-    const endDate = new Date(budgetEndDate);
+  // // ...
 
-    if (currentDate >= startDate && currentDate <= endDate) {
-      const totalDays =
-        (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
-      const elapsedDays =
-        (currentDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
-      progress = (elapsedDays / totalDays) * 100;
-    }
-  }
+  // if (budgetStartDate && budgetEndDate) {
+  //   const startDate = startOfDay(new Date(budgetStartDate)); // Convert to start of day in UTC
+  //   const endDate = startOfDay(new Date(budgetEndDate)); // Convert to start of day in UTC
+  //   const currentDate = new Date(); // Convert to start of day in UTC
+
+  //   if (currentDate >= startDate) {
+  //     const totalMillis = differenceInMilliseconds(endDate, startDate);
+  //     const elapsedMillis = differenceInMilliseconds(currentDate, startDate);
+
+  //     progress = (elapsedMillis / totalMillis) * 100;
+  //   }
+  // }
+  // console.log("Progress value", progress.toFixed(2));
+
+  const budgetExpenses = expenses?.filter((expense) => {
+    return budgetStartDate && expense.date >= budgetStartDate;
+  });
+
+  const budgetExpenseTotal = budgetExpenses?.reduce((acc, expense) => {
+    return (acc += parseInt(expense.amount));
+  }, 0);
 
   return (
-    <Box>
-      <HStack>
+    <>
+      <Box textAlign="center">
         <Stat>
-          <StatLabel>{budget?.[0]?.startDate}</StatLabel>
-          <StatNumber>£0.00</StatNumber>
+          <StatLabel fontWeight="bold" fontSize="md" color="brand.text">
+            Budget set at
+          </StatLabel>
+          <StatNumber>
+            ${budgetAmount && parseInt(budgetAmount).toFixed(2)}
+          </StatNumber>
         </Stat>
-        <Progress hasStripe value={progress} />
-      </HStack>
-    </Box>
+      </Box>
+      <Divider />
+      <Flex justify={"space-evenly"}>
+        <Box>
+          <Text color="brand.text" fontWeight="bold">
+            Start Date
+          </Text>
+          <Text fontSize="xl">{budgetStartDate}</Text>
+        </Box>
+
+        <div>
+          <Text color="brand.text" fontWeight="bold">
+            End Date
+          </Text>
+          <Text fontSize="xl">{budgetEndDate}</Text>
+        </div>
+      </Flex>
+      <Center>
+        <CircularProgress
+          size="250px"
+          value={40}
+          color={
+            budgetExpenseTotal &&
+            budgetAmount &&
+            parseInt(budgetAmount) >= budgetExpenseTotal
+              ? "green.500"
+              : "red.500"
+          }
+        >
+          <CircularProgressLabel fontSize="xl">
+            <Stack>
+              <Text>Expense Total :</Text>
+              <Text>${budgetExpenseTotal?.toFixed(2)}</Text>
+            </Stack>
+          </CircularProgressLabel>
+        </CircularProgress>
+      </Center>
+    </>
   );
 };
--- src/pages/UserBudget.tsx
+++ src/pages/UserBudget.tsx
@@ -1,11 +1,13 @@
 import { BudgetTable } from "../components/Budgeting/BudgetTable";
 import { BudgetForm } from "../components/Budgeting/BudgetForm";
+import { BudgetPlan } from "../components/Budgeting/BudgetPlan";
 
 export const UserBudget = () => {
   return (
     <div>
       <BudgetForm />
       <BudgetTable />
+      <BudgetPlan />
     </div>
   );
 };
